6ab0448d70ee3a6163fa39b2c2cd19f6c1591bbf
Seungtae Heo <Seungtae Heo>
2017-06-09 01:43:38 +0900

    init Heroku

--- config/passport/.js
+++ config/passport/.js
@@ -1,20 +1,20 @@
 /**
  * 패스포트 설정 파일
  * 
- * 페이스북 인증 방식에 사용되는 패스포트 설정
+ * 구글 인증 방식에 사용되는 패스포트 설정
  *
  * @date 2016-11-10
  * @author Mike
  */
 
-var NaverStrategy = require('passport-naver').Strategy;
+var GoogleStrategy = require('passport-naver').Strategy;
 var config = require('../config');
 
 module.exports = function(app, passport) {
 	return new NaverStrategy({
-		clientID: config.naver.clientID,
-		clientSecret: config.naver.clientSecret,
-		callbackURL: config.naver.callbackURL
+    	clientID: config.naver.clientID,
+    	clientSecret: config.naver.clientSecret,
+    	callbackURL: config.naver.callbackURL
 	}, function(accessToken, refreshToken, profile, done) {
 		console.log('passport의 naver 호출됨.');
 		console.dir(profile);
@@ -30,10 +30,9 @@ module.exports = function(app, passport) {
 			if (!user) {
 				var user = new database.UserModel({
 					name: profile.displayName,
-					email: profile.emails[0].value,
-					provider: 'naver',
-					authToken: accessToken,
-					facebook: profile._json
+			        email: profile.emails[0].value,
+			        provider: 'naver',
+			        google: profile._json
 				});
         
 				user.save(function (err) {
